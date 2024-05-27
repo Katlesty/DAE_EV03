65d2ef389697d14ad4cc7aaae607af9025e20bc4
Katlesty <yahaira.chavez@tecsup.edu.pe>
2024-05-27 17:30:22 -0500

    dsa

--- EV02/biblioteca/views.py
+++ EV02/biblioteca/views.py
@@ -40,93 +40,93 @@ class LibroView(APIView):
     def get(self, request):
         libros = Libro.objects.all()
         serLibro = LibroSerializer(libros, many=True)
-        return Response(serializer.data)
+        return Response(serLibro.data)
     
     def post(self, request):
         serLibro = LibroSerializer(data=request.data)
         serLibro.is_valid(raise_exception=True)
         serLibro.save()
-        return Response(serializer.data)
+        return Response(serLibro.data)
 
 class LibroDetalle(APIView):
     def get(self, request, libro_id):
         libro = Libro.objects.get(idLibro=libro_id)
         serLibro = LibroSerializer(libro)
-        return Response(serializer.data)
+        return Response(serLibro.data)
     
     def put(self, request, libro_id):
         libro = Libro.objects.get(idLibro=libro_id)
         serLibro = LibroSerializer(libro, data=request.data)
         serLibro.is_valid(raise_exception=True)
         serLibro.save()
-        return Response(serializer.data)
+        return Response(serLibro.data)
 
     def delete(self, request, libro_id):
         libro = Libro.objects.get(idLibro=libro_id)
         serLibro = LibroSerializer(libro)
         libro.delete()
-        return Response(serializer.data)
+        return Response(serLibro.data)
 
 class UsuarioView(APIView):
     def get(self, request):
         usuarios = Usuario.objects.all()
         serUsuario = UsuarioSerializer(usuarios, many=True)
-        return Response(serializer.data)
+        return Response(serUsuario.data)
     
     def post(self, request):
         serUsuario = UsuarioSerializer(data=request.data)
         serUsuario.is_valid(raise_exception=True)
         serUsuario.save()
-        return Response(serializer.data)
+        return Response(serUsuario.data)
 
 class UsuarioDetalle(APIView):
     def get(self, request, usuario_id):
         usuario = Usuario.objects.get(idUsuario=usuario_id)
         serUsuario = UsuarioSerializer(usuario)
-        return Response(serializer.data)
+        return Response(serUsuario.data)
     
     def put(self, request, usuario_id):
         usuario = Usuario.objects.get(idUsuario=usuario_id)
         serUsuario = UsuarioSerializer(usuario, data=request.data)
         serUsuario.is_valid(raise_exception=True)
         serUsuario.save()
-        return Response(serializer.data)
+        return Response(serUsuario.data)
 
     def delete(self, request, usuario_id):
         usuario = Usuario.objects.get(idUsuario=usuario_id)
         serUsuario = UsuarioSerializer(usuario)
         usuario.delete()
-        return Response(serializer.data)
+        return Response(serUsuario.data)
 
 class PrestamoView(APIView):
     def get(self, request):
-        prestamos = Prestamo.objects.all()
-        serPrestamos = PrestamoSerializer(prestamos, many=True)
-        return Response(serializer.data)
+        prestamos = Prestamos.objects.all()
+        serPrestamos = PrestamosSerializer(prestamos, many=True)
+        return Response(serPrestamos.data)
     
     def post(self, request):
-        serPrestamos = PrestamoSerializer(data=request.data)
+        serPrestamos = PrestamosSerializer(data=request.data)
         serPrestamos.is_valid(raise_exception=True)
         serPrestamos.save()
-        return Response(serializer.data)
+        return Response(serPrestamos.data)
 
 class PrestamoDetalle(APIView):
     def get(self, request, prestamo_id):
-        prestamo = Prestamo.objects.get(idPrestamo=prestamo_id)
+        prestamo = Prestamos.objects.get(idPrestamo=prestamo_id)
         serPrestamos = PrestamoSerializer(prestamo)
-        return Response(serializer.data)
+        return Response(serPrestamos.data)
     
     def put(self, request, prestamo_id):
-        prestamo = Prestamo.objects.get(idPrestamo=prestamo_id)
-        serPrestamos = PrestamoSerializer(prestamo, data=request.data)
+        prestamo = Prestamos.objects.get(idPrestamo=prestamo_id)
+        serPrestamos = PrestamosSerializer(prestamo, data=request.data)
         serPrestamos.is_valid(raise_exception=True)
         serPrestamos.save()
-        return Response(serializer.data)
+        return Response(serPrestamos.data)
 
     def delete(self, request, prestamo_id):
-        prestamo = Prestamo.objects.get(idPrestamo=prestamo_id)
-        serPrestamos = PrestamoSerializer(prestamo)
+        prestamo = Prestamos.objects.get(idPrestamo=prestamo_id)
+        serPrestamos = PrestamosSerializer(prestamo)
         prestamo.delete()
-        return Response(serializer.data)
+        return Response(serPrestamos.data)
 
 
